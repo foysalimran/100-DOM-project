649ddb05faefb63887038f6305853ccfed03afda
foysalimran <fimran2011@gmail.com>
2022-05-19 08:26:36 +0600

    adding stepes

--- project-4/script.js
+++ project-4/script.js
@@ -1,15 +1,23 @@
+/**
+ * Project Requirements:
+ * - Change teh background color by generating random hex color by clicking a button
+ * - Also display the hex code to a disabled input field
+ * Add a button to copy the color code
+ * Add a toast message when copied
+ */
 
+// Steps
+
+// Step 1 - Create on load handler
 window.onload = () => (
     main()
 )
 
 function main() {
-
     let changeBtn = document.getElementById('change-btn');
     let output = document.getElementById('output');
     let btnCopy = document.getElementById('copy-btn')
     let root = document.getElementById('root');
-
 
     changeBtn.addEventListener('click', function() {
         const bgColor = generateRGBcode()
@@ -31,3 +39,15 @@ function generateRGBcode() {
     let blue = Math.floor(Math.random() * 255);
     return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`
 }
+
+// Step 3 - collect all necessary refactors
+
+// Step 4 - Handle the change button click event
+
+// Step 5 - handle the copy button click event
+
+// Step 6 - Activated toast message
+
+// Step 7 - Create dynamic toast message
+
+// Step 8 - Clear toast message
